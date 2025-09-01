f8919e11a9880a9a054f949e9f14ea56c773f962
김지욱 <dragonjiwook2012@gmail.com>
2025-09-02 07:27:50 +0900

    Update BOJ2470.py

--- BOJ2470.py
+++ BOJ2470.py
@@ -1,20 +1,20 @@
 n=int(input())
 l=list(map(int,input().split()))
 l.sort()
-best=float('inf')
-s,e=0,n-1
+best=float('inf')#가장 좋은 값은 일단 무한
+s,e=0,n-1#맨 앞과 맨 뒤
 ans=(l[s],l[e])
 while s<e:
     total=l[s]+l[e]
-    if(abs(total)<abs(best)):
+    if(abs(total)<abs(best)):#만일 best보다 지금이 더 좋으면
         best=total
-        ans=(l[s],l[e])
-    if(total>0):
-        e-=1
-    elif total<0:
-        s+=1
+        ans=(l[s],l[e])#값 변경
+    if(total>0):#만일 양수면
+        e-=1#끝을 한칸 앞당기고
+    elif total<0:#음수면
+        s+=1#시작을 한칸 뒤로
     else:
-        break
+        break#0일때는 최선으로 그냥 바로 break
 print(ans[0],ans[1])
 #백준 2470
 #두 용액
