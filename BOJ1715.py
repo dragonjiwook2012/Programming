c48b152adeaa6074ab4ff618621bfaeaf2907021
김지욱 <dragonjiwook2012@gmail.com>
2025-09-02 07:26:27 +0900

    Update BOJ1715.py

--- BOJ1715.py
+++ BOJ1715.py
@@ -7,10 +7,10 @@ ans=0
 for _ in range(n):
     heapq.heappush(cards,int(input().rstrip()))
 while len(cards)>=2:
-    a=heapq.heappop(cards)
-    b=heapq.heappop(cards)
-    ans+=a+b
-    heapq.heappush(cards,a+b)
+    a=heapq.heappop(cards)#가장 작은 카드 장수 고르고
+    b=heapq.heappop(cards)#또 가장 작은 카드 장수를 고름
+    ans+=a+b#더해서
+    heapq.heappush(cards,a+b)#넣으면 됨
 print(ans)
 #백준 1715
 #카드정렬하기
