2d38ded9eb7674b68fc36d19aaa4e24a2b9e24e5
김지욱 <dragonjiwook2012@gmail.com>
2025-09-14 13:33:29 +0900

    Create BOJ14921.py

---
+++ BOJ14921.py
@@ -0,0 +1,17 @@
+n=int(input())
+l=list(map(int,input().split()))
+s,e=0,n-1
+best=float('inf')
+while s<e:
+    if(abs(l[s]+l[e])<abs(best)):
+        best=l[s]+l[e]
+    if(l[s]+l[e]<0):
+        s+=1
+        if(s>=e):
+            break
+    else:
+        e-=1
+print(best)
+#용액 합성하기
+#골드 4
+#전형적인 투 포인터문제
